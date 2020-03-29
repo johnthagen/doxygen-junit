9972229280f8fa235a9fa16d3d0424a5c6e62e15
johnthagen <johnthagen@gmail.com>
2020-03-29 12:25:11 -0400

    Add Python 3 Only trove classifier

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
         'Programming Language :: C++',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
